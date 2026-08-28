fa515fe9f907e8cc2accf38f60e704808e901e08
HirunaWeerakoon <it24610788@my.sliit.lk>
2026-08-28 18:49:58 +0530

    added version to test the dev branch actions

--- Arena/services/user/src/Controllers/HealthController.cs
+++ Arena/services/user/src/Controllers/HealthController.cs
@@ -9,6 +9,6 @@ public class HealthController : ControllerBase
     [HttpGet]
     public IActionResult Get()
     {
-        return Ok(new { status = "healthy", service = "user" });
+        return Ok(new { status = "healthy", service = "user",version = "1.0.0" });
     }
-}
+} // the version in the response it added to test the ci part. delete when merging.
